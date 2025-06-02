6f3b15f6d22221f0f94aabb6a54c6113e1f39620
Jaffer <aljarallajaffer@gmail.com>
2025-06-02 18:04:48 +0200

    removed comment

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,8 +1,6 @@
 import React, { useRef } from "react";
 import "./styles.css";
 
-// minute 40 of tutorial
-
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
